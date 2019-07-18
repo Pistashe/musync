d01844bb8e1055a6077f792d9ab155c87723c5bf
sgiorno <sgiorno@localhost.localdomain>
2019-07-18 17:34:39 +0200

    Ajout de rsync

--- app.py
+++ app.py
@@ -12,25 +12,44 @@ from kivy.uix.gridlayout import GridLayout
 
 from ssh_client import SSHClient
 from file_system_remote import FileSystemRemote
+import subprocess as sp
+import re
 
 kivy.require("1.9.1")
 
+
+SOURCE_PATH = "/home/sgiorno/musync_env/musync/tests/environnement/source"
+TARGET_PATH = "/home/sgiorno/musync_env/musync/tests/environnement/target"
+
+def _find_text_between_patterns(text, pat1, pat2):
+    return re.findall(r'{}(.*?){}'.format(pat1, pat2), text)
+
+def _parser(text):
+    print(_find_text_between_patterns(text, "cd+++++++++", "\n"))
 
 # class in which we are creating the button
 class Musync(App):
     def build(self):
         # layout = BoxLayout(orientation='horizontal')
         layout = GridLayout(rows=2)
-        lab1 = Label(text="[b]Local[/b]", height=5, markup=True)
-        lab2 = Label(text="[b]Remote[/b]", height=5, markup=True)
+        lab1 = Label(text="Local" , height=30, bold=True, size_hint_y=None)
+        lab2 = Label(text="Remote", height=30, bold=True, size_hint_y=None)
         layout.add_widget(lab1)
         layout.add_widget(lab2)
         ssh_client = SSHClient("sgiorno", "192.168.122.1", 22, "babibel123")
         file_system_remote = FileSystemRemote(ssh_client)
-        # file_chooser_source = FileChooserListView(path="/home/sgiorno")
-        # file_chooser_target = FileChooserListView(file_system=file_system_remote, path="/home/sgiorno")
-        # layout.add_widget(file_chooser_source)
-        # layout.add_widget(file_chooser_target)
+        file_chooser_source = FileChooserListView(path=SOURCE_PATH)
+        file_chooser_target = FileChooserListView(file_system=file_system_remote,
+                                                  path=TARGET_PATH)
+        # proc = sp.Popen(["rsync", "-ain", "--del", SOURCE_PATH,
+        #          "sgiorno@192.168.122.1:{}".format(TARGET_PATH)],
+        #         stdout=sp.PIPE)
+        # text = proc.communicate()[0]
+        # text = text.decode("utf-8")
+        # print(text)
+        # _parser(text)
+        layout.add_widget(file_chooser_source)
+        layout.add_widget(file_chooser_target)
         btn = Button(text="Push Me !",
                      font_size="20sp",
                      background_color=(1, 1, 1, 1),
@@ -40,8 +59,8 @@ class Musync(App):
                      pos=(300, 250))
 
         btn.bind(on_press=self.callback)
-        self.load_kv("ui.kv")
-        # return layout
+        # self.load_kv("ui.kv")
+        return layout
 
     # callback function tells when button pressed
     def callback(self, event):
---
+++ tests/reset.py
@@ -0,0 +1,30 @@
+import os
+from shutil import rmtree, copyfile
+
+root   = "environnement"
+source = root + "/source"
+target = root + "/target"
+
+rmtree(root)
+
+os.mkdir(root)
+os.mkdir(source)
+os.mkdir(target)
+
+for folder in ["folder1", "folder2"]:
+    os.mkdir(source + "/" + folder)
+    for file_name in ["file1", "file2", "file3"]:
+        path = os.path.join(source, folder, file_name)
+        with open(path, "w") as f:
+            f.write("okkk")
+
+for folder in ["folder1"]:
+    os.mkdir(target + "/" + folder)
+    os.mkdir(target + "/ok")
+    path = os.path.join(target, "ok", "fiiile")
+    with open(path, "w") as f:
+        f.write("okkk")
+    for file_name in ["file1"]:
+        path = os.path.join(target, folder, file_name)
+        with open(path, "w") as f:
+            f.write("okkk")
