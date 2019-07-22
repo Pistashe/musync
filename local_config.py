ff7e439b54f93b561300d4bd7c79a0a8fce507b2
sgiorno <sgiorno@localhost.localdomain>
2019-07-22 10:32:08 +0200

    Removal of useless files

--- local_config.py
+++
@@ -1,9 +0,0 @@
-# SSH config
-UNAME = "bob"
-IP    = "192.168.0.18"
-PORT  = 1234
-PWD   = "babibel123"
-
-# Test config
-SOURCE_PATH = "/home/sven/Projets/musync_env/musync/tests/environnement/source"
-TARGET_PATH = "/home/sven/Projets/musync_env/musync/tests/environnement/target"
